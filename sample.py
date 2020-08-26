376f551c71fa54ce25f929fba977c61fbe15879a
harshagr18 <harsh.agrawal17@siesgst.ac.in>
2020-08-26 11:21:36 +0530

    Updating sample

--- sample.py
+++ sample.py
@@ -1,5 +1,7 @@
-x = int(input())
-for i in range(x):
+# Problem Link : 
+
+u = int(input())
+for _ in range(u):
     a = int(input())
     a,b = list(map(int, input().split(" ")))
     a = list(map(int,input().split()))
