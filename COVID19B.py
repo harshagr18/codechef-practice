8864053c721ecb3d650fe8b28053d4acdd564e29
harshagr18 <harsh.agrawal17@siesgst.ac.in>
2020-09-08 10:27:04 +0530

    Trial

---
+++ COVID19B.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.codechef.com/SEPT20B/problems/COVID19B
+
+u = int(input())
+for _ in range(u):
+    a = int(input())
+    a,b = list(map(int, input().split(" ")))
+    a = list(map(int,input().split()))
