e29a79c9993e109f2a03d51361940a88f821070a
Romeo2136 <vchoupane8@gmail.com>
2021-10-05 00:09:37 -0400

    Updated File list

---
+++ exceptions.py
@@ -0,0 +1,9 @@
+# Problem Link : https://www.hackerrank.com/challenges/exceptions/problem
+
+lines = int(input())
+for _ in range(lines):
+    a, b = input().split()
+    try:
+        print(int(a)//int(b))
+    except (ZeroDivisionError, ValueError) as e:
+        print("Error Code:", e)
