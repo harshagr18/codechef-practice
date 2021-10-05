e858b60c00141ad7468eba49e3adc26b8020615c
dsikligar <sikligardhaval@gmail.com>
2021-10-05 00:08:48 -0400

    Commit message

--- COVID19B.py
+++ COVID19B.py
@@ -1,7 +1,61 @@
-# Problem Link : https://www.codechef.com/SEPT20B/problems/COVID19B
+# Python program for implementation of MergeSort
+def mergeSort(arr):
+	if len(arr) > 1:
 
-u = int(input())
-for _ in range(u):
-    a = int(input())
-    a,b = list(map(int, input().split(" ")))
-    a = list(map(int,input().split()))
+		# Finding the mid of the array
+		mid = len(arr)//2
+
+		# Dividing the array elements
+		L = arr[:mid]
+
+		# into 2 halves
+		R = arr[mid:]
+
+		# Sorting the first half
+		mergeSort(L)
+
+		# Sorting the second half
+		mergeSort(R)
+
+		i = j = k = 0
+
+		# Copy data to temp arrays L[] and R[]
+		while i < len(L) and j < len(R):
+			if L[i] < R[j]:
+				arr[k] = L[i]
+				i += 1
+			else:
+				arr[k] = R[j]
+				j += 1
+			k += 1
+
+		# Checking if any element was left
+		while i < len(L):
+			arr[k] = L[i]
+			i += 1
+			k += 1
+
+		while j < len(R):
+			arr[k] = R[j]
+			j += 1
+			k += 1
+
+# Code to print the list
+
+
+def printList(arr):
+	for i in range(len(arr)):
+		print(arr[i], end=" ")
+	print()
+
+
+# Driver Code
+if __name__ == '__main__':
+	arr = [12, 11, 13, 5, 6, 7]
+	print("Given array is", end="\n")
+	printList(arr)
+	mergeSort(arr)
+	print("Sorted array is: ", end="\n")
+	printList(arr)
+
+# This code is contributed by Mayank Khanna
